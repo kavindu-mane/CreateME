2d328fba3805b63b70cf280119c069aee4d1a31d
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2023-03-09 22:12:46 +0530

    updates 03/09

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,11 +1,21 @@
 import React , {Component} from "react";
+import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
+import remarkGfm from "remark-gfm";
 import "../resources/styles.css"
+import MarkDown from "./Markdown.md"
 
 class Create extends Component {
-    render() { 
+    render() {
+
         return (
             <React.Fragment>
-               
+                <diw className="container-fluid mt-5 row mx-0 px-4">
+                    <div className="col-lg-6 px-3 mt-5 bg-secondary rounded">aaaaaaa</div>
+                    <div className="col-lg-6 px-3 mt-5 bg-primary rounded" >
+                        <ReactMarkdown children={MarkDown} rehypePlugins={[rehypeRaw]}
+                        remarkPlugins= {[remarkGfm]}  /></div>
+                </diw>
             </React.Fragment>
         );
     }
---
+++ src/pages/Markdown.md
@@ -0,0 +1,12 @@
+A paragraph with *emphasis* and **strong importance** 
+
+> A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+
+* Lists
+    * [ ] todo
+    * [x] done
+
+A table:
+
+| a | b |
+| - | - |
